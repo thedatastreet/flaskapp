d567cfae2a74db25a9032bacfda48f55086c0d4d
thedatastreet <thedatastreet@gmail.com>
2019-10-21 10:05:09 +0530

    adding index and about pages

--- routes.py
+++ routes.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    
-    return "<html><h1>Hello, Universe!</h1></html>"
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
