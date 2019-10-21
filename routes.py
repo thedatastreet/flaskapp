dd268a226f203cf8cc9e4d80ae75976df404b19e
thedatastreet <thedatastreet@gmail.com>
2019-10-21 09:52:55 +0530

    updating routes with html

--- routes.py
+++ routes.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    
+    return "<html><h1>Hello, Universe!</h1></html>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
